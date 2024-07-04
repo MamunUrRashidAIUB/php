dd9eb4909859805628d33dba065948445099a4ed
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-07-04 22:37:47 +0600

    removes the unnecessary elements

--- library.php
+++ library.php
@@ -1,23 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>library</title>
+  <title>Library</title>
   <style>
     .container {
       display: flex;
-      justify-content: center;
+      flex-direction: column;
       align-items: center;
-       
-      flex-direction: column; 
     }
 
-
-    .flex-container {
-      display: flex;
-      gap: 50px;
-    }
-
-    .flex-container2 {
+    .flex-container, .flex-container2 {
       display: flex;
       gap: 50px;
     }
@@ -25,6 +17,7 @@
     .text {
       display: flex;
       gap: 210px;
+      justify-content: center;
     }
 
     .error {
@@ -33,7 +26,6 @@
 
     ul {
       list-style-type: none;
-      margin: 10px;
       padding: 0;
       text-align: center;
     }
@@ -43,8 +35,8 @@
       padding: 10px;
     }
 
-    h1.text-5xl {
-      text-align: center; 
+    h1 {
+      text-align: center;
     }
   </style>
 </head>
@@ -75,14 +67,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $id = test_input($_POST["id"]);
     }
 
-    
     if (isset($_SESSION['borrowed']) && $_SESSION['borrowed'] == true) {
         $bnameErr = "You have already borrowed a book";
     } else {
-        
         $_SESSION['borrowed'] = true;
 
-      
         $cookie_name = "library_data";
         $cookie_value = json_encode([
             'bname' => $bname,
@@ -94,87 +83,72 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    return htmlspecialchars(stripslashes(trim($data)));
 }
 ?>
 
 <div class="container">
-
   <ul>
     <li><a href="#">Login</a></li>
     <li><a href="#">Token validation</a></li>
-    <li><a href="#">book search</a></li>
+    <li><a href="#">Book search</a></li>
     <li><a href="#">Book loan</a></li>
-    <li><a href="#">send reminder</a></li>
+    <li><a href="#">Send reminder</a></li>
   </ul>
-  
-  <h1 class="text-5xl">Need any book??</h1>
 
-  <div class="flex-container place-content-center">
+  <h1>Need any book?</h1>
+
+  <div class="flex-container">
     <img src="images/1.jpg" alt="Book 1" height="200" width="200">
     <img src="images/2.jpg" alt="Book 2" height="200" width="200">
     <img src="images/3.jpg" alt="Book 3" height="200" width="200">
   </div>
 
-  <div class="text place-content-center">
+  <div class="text">
     <p>Design</p>
     <p>Annual Report</p>
     <p>Amphibious Soul</p>
   </div>
 
-  <div class="flex-container2 place-content-center">
+  <div class="flex-container2">
     <img src="images/4.jpg" alt="Book 4" height="200" width="200">
     <img src="images/5.jpg" alt="Book 5" height="200" width="200">
     <img src="images/6.jpg" alt="Book 6" height="200" width="200">
   </div>
 
-  <div class="text place-content-center">
-    <p>I hope this finds you well</p>
+  <div class="text">
+    <p>I Hope This Finds You Well</p>
     <p>Happy</p>
     <p>Soul River</p>
   </div>
-  
+
   <div>
-    <h1 class="text-4xl">Search a book</h1>
-    <p class="text place-content-center"><span class="error">* required field</span></p>
-    <div>
-      <form class="place-content-center" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-
-        <label>Enter Book name:</label><br>
-        <input type="text" name="bname">
-        <span class="error">* <?php echo $bnameErr;?></span>
-        <br>
-        <label>Enter your name</label>
-        <br>
-        <input type="text"  name="name">
-        <span class="error">* <?php echo $nameErr;?></span>
-        <br>
-        <label>Enter Your Student Id:</label>
-        <br>
-        <input type="text" name="id">
-        <span class="error">* <?php echo $idErr;?></span>
-        <br>
-
-        <br>
-        <input type="submit" value="Submit">
-      </form>
-    </div>
+    <h1>Search a book</h1>
+    <p><span class="error">* required field</span></p>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+      <label>Enter Book Name:</label><br>
+      <input type="text" name="bname">
+      <span class="error">* <?php echo $bnameErr;?></span>
+      <br>
+      <label>Enter Your Name:</label><br>
+      <input type="text" name="name">
+      <span class="error">* <?php echo $nameErr;?></span>
+      <br>
+      <label>Enter Your Student ID:</label><br>
+      <input type="text" name="id">
+      <span class="error">* <?php echo $idErr;?></span>
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
   </div>
 
   <?php
-   echo "<h2> show data:</h2>";
-   echo $bname;
-   echo "<br>";
-   echo $name;
-   echo "<br>";
-   echo $id;
-   echo "<br>";
-   $cookie_name = "library_data";
+  echo "<h2>Show Data:</h2>";
+  echo $bname . "<br>";
+  echo $name . "<br>";
+  echo $id . "<br>";
 
-   if (isset($_COOKIE[$cookie_name])) {
+  if (isset($_COOKIE[$cookie_name])) {
     $stored_data = json_decode($_COOKIE[$cookie_name], true);
     echo "<h2>Stored Data:</h2>";
     echo "Book Name: " . $stored_data['bname'] . "<br>";
