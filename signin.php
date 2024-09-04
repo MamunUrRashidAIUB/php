4faf1b62c8e8d16ad69fb588d7d176a63d4ef62c
Mamun ur rashid <md.mamun.ur.rashid.cse@gmail.com>
2024-09-05 01:33:30 +0600

    added forgot password

--- signin.php
+++ signin.php
@@ -22,10 +22,20 @@
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
             width: 400px;
         }
-        .fullbody{
-
-background-color:#E1DCC5;
-}
+        .fullbody {
+            background-color: #E1DCC5;
+        }
+        .forgot-password {
+            text-align: center;
+            margin-top: 10px;
+        }
+        .forgot-password a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        .forgot-password a:hover {
+            text-decoration: underline;
+        }
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -46,6 +56,9 @@ background-color:#E1DCC5;
             </div>
             <button type="submit" name="submit" class="btn btn-primary">Login</button>
         </form>
+        <div class="forgot-password">
+            <a href="forgotpassword.php">Forgot Password?</a>
+        </div>
     </div>
 </div>
 
